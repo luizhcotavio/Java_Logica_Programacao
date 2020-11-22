6dcf84ee2cdac70de193adacc14172533ab77585
Luiz Otávio <51968457+luizhcotavio@users.noreply.github.com>
2020-11-22 18:54:13 -0300

    Update CalculaBonusPorMeta.java

--- src/CalculaBonusPorMeta.java
+++ src/CalculaBonusPorMeta.java
@@ -3,12 +3,7 @@ import java.util.Scanner;
 public class CalculaBonusPorMeta {
 
 	public static void main(String[] args) {
-		
-		// Informar Valor da Meta de faturamento do ano passado
-		// Informar valor do faturamento real do ultimo ano
-		// Informar Media Salarial do ano anterior
-		
-		
+			
 		
 		Scanner leia = new Scanner(System.in);
 		
@@ -21,10 +16,10 @@ public class CalculaBonusPorMeta {
 		System.out.print("Informe o valor da Meta de Faturamento do ano passado: ");
 		META_FATURAMENTO_ANO_PASSADO = leia.nextDouble();
 		
-		System.out.print("Informe o valor do Faturamento Real do ⁄ltimo Ano: ");
+		System.out.print("Informe o valor do Faturamento Real do √öltimo Ano: ");
 		VALOR_FATURAMENTO_REAL = leia.nextDouble();
 		
-		System.out.print("Informe a Media Salarial do ⁄ltimo Ano: ");
+		System.out.print("Informe a Media Salarial do √öltimo Ano: ");
 		MEDIA_SALARIAL_ANO_PASSADO = leia.nextDouble();
 		
 		CALCULA_PORCENTAGEM_DA_META = 80* (META_FATURAMENTO_ANO_PASSADO/100);
@@ -32,11 +27,11 @@ public class CalculaBonusPorMeta {
 		boolean FATURAMENTO_MENOR_QUE_META_MAS_ATINGIU_PORCENTAGEM = (VALOR_FATURAMENTO_REAL < META_FATURAMENTO_ANO_PASSADO) && (VALOR_FATURAMENTO_REAL >= CALCULA_PORCENTAGEM_DA_META);
 		
 		if(FATURAMENTO_REAL_MAIOR_QUE_META) {
-			System.out.println("Os funcion·rios receber„o um bonus salarial no valor de R$"+ MEDIA_SALARIAL_ANO_PASSADO+" Reais.");
+			System.out.println("Os funcion√°rios receber√£o um bonus salarial no valor de R$"+ MEDIA_SALARIAL_ANO_PASSADO+" Reais.");
 		}else if (FATURAMENTO_MENOR_QUE_META_MAS_ATINGIU_PORCENTAGEM) {
-			System.out.println("Os funcion·rios receber„o uma media salarial no valor de R$"+ CALCULA_PORCENTAGEM_DA_META +" Reais.");
+			System.out.println("Os funcion√°rios receber√£o uma media salarial no valor de R$"+ CALCULA_PORCENTAGEM_DA_META +" Reais.");
 		}else {
-			System.out.println("Os funcion·rios n„o receber„o bonus.");
+			System.out.println("Os funcion√°rios n√£o receber√£o bonus.");
 		}
 		
 		
