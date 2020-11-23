72dee68955b32859bafa301ad91942581bc86dfd
Luiz Otávio <51968457+luizhcotavio@users.noreply.github.com>
2020-11-23 10:12:14 -0300

    Update CalculaAposentadoria.java

--- src/CalculaAposentadoria.java
+++ src/CalculaAposentadoria.java
@@ -5,20 +5,20 @@ public class CalculaAposentadoria {
 	static final Double TEMPO_MINIMO_CONTRIBUICAO_PARA_APOSENTAR = 25.0;
 	
 	public static void main(String[] args) {
-		// idadePessoa >= 55 && contribuicao >25
+		
 		
 		Scanner leia = new Scanner (System.in);
-		Double IDADE_PESSOA = null;
-		Double TEMPO_DE_CONTRIBUICAO = null;
+		Double idadePessoa= null;
+		Double tempoDeContribuicao = null; 
 		
 		
 		System.out.println("Entre com a sua idade: ");
-		IDADE_PESSOA = leia.nextDouble();
+		idadePessoa = leia.nextDouble();
 		
 		System.out.println("Entre com o tempo de contribuição: ");
-		TEMPO_DE_CONTRIBUICAO = leia.nextDouble();
+		tempoDeContribuicao = leia.nextDouble();
 		
-		if(IDADE_PESSOA >= IDADE_MINIMA_PARA_APOSENTAR && TEMPO_DE_CONTRIBUICAO >= TEMPO_MINIMO_CONTRIBUICAO_PARA_APOSENTAR) {
+		if(idadePessoa >= IDADE_MINIMA_PARA_APOSENTAR && tempoDeContribuicao >= TEMPO_MINIMO_CONTRIBUICAO_PARA_APOSENTAR) {
 			System.out.println("Pode Aposentar");
 		}else {
 			System.out.println("Não pode aposentar");
